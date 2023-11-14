2bd4604d5901ccfab43e3a6524631074ebc11e41
Helen Esterman <helenesterman99@gmail.com>
2023-11-14 16:40:21 -0500

    inital commit

---
+++ src/pages/Login.jsx
@@ -0,0 +1,94 @@
+import { useRef, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import "../CSS/Login.CSS";
+
+export const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const existDialog = useRef();
+  const navigate = useNavigate();
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    fetch(`http://localhost:8000/login`, {
+      method: "POST",
+      body: JSON.stringify({ email, password }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((authInfo) => {
+        //TODO: .valid might be a different key on your object and rock_token doesn't make sense so that will also need to change
+        if (authInfo.valid) {
+          localStorage.setItem("rock_token", JSON.stringify(authInfo));
+          navigate("/");
+        } else {
+          existDialog.current.showModal();
+        }
+      });
+  };
+
+  return (
+    <main className="container--login">
+      <dialog className="dialog dialog--auth" ref={existDialog}>
+        <div>User does not exist</div>
+        <button
+          className="button--close"
+          onClick={() => existDialog.current.close()}
+        >
+          Close
+        </button>
+      </dialog>
+
+      <section>
+        <form className="form--login" onSubmit={handleLogin}>
+          <h1 className="text-4xl mt-7 mb-3">Level Up</h1>
+          <h2 className="text-xl mb-10">Please sign in</h2>
+          <fieldset className="mb-4">
+            <label htmlFor="inputEmail"> Email address </label>
+            <input
+              type="email"
+              id="inputEmail"
+              value={email}
+              onChange={(evt) => setEmail(evt.target.value)}
+              className="form-control"
+              placeholder="Email address"
+              required
+              autoFocus
+            />
+          </fieldset>
+          <fieldset className="mb-4">
+            <label htmlFor="inputPassword"> Password </label>
+            <input
+              type="password"
+              id="inputPassword"
+              value={password}
+              onChange={(evt) => setPassword(evt.target.value)}
+              className="form-control"
+              placeholder="Password"
+            />
+          </fieldset>
+          <fieldset>
+            <button
+              type="submit"
+              className="button p-3 rounded-md bg-blue-800 text-blue-100"
+            >
+              Sign in
+            </button>
+          </fieldset>
+        </form>
+      </section>
+      <div className="loginLinks">
+        <section className="link--register">
+          <Link
+            className="underline text-blue-600 hover:text-blue-800 visited:text-purple-600"
+            to="/register"
+          >
+            Not a member yet?
+          </Link>
+        </section>
+      </div>
+    </main>
+  );
+};
